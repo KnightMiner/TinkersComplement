da42189ca45ba8b54dbc13133dd222d97ae0ec0f
KnightMiner <knightminer4@gmail.com>
2017-08-20 15:13:19 -0500

    Add recipe lookup to the melter GUI

--- src/main/java/knightminer/tcomplement/plugin/jei/JEIPlugin.java
+++ src/main/java/knightminer/tcomplement/plugin/jei/JEIPlugin.java
@@ -1,26 +1,24 @@
 package knightminer.tcomplement.plugin.jei;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import knightminer.tcomplement.TinkersComplement;
 import knightminer.tcomplement.feature.ModuleFeature;
-import mezz.jei.api.IJeiRuntime;
+import knightminer.tcomplement.feature.client.GuiMelter;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
-import mezz.jei.api.ISubtypeRegistry;
-import mezz.jei.api.ingredients.IModIngredientRegistration;
+import mezz.jei.api.gui.IAdvancedGuiHandler;
 import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.item.ItemStack;
+import slimeknights.tconstruct.smeltery.client.IGuiLiquidTank;
 
 
 @mezz.jei.api.JEIPlugin
 public class JEIPlugin implements IModPlugin {
 	private static final String FURNACE_FUEL = VanillaRecipeCategoryUid.FUEL;
 	private static final String TINKERS_SMELTERY = "tconstruct.smeltery";
-
-	@Override
-	public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {}
-
-	@Override
-	public void registerIngredients(IModIngredientRegistration registry) {}
 
 	@Override
 	public void register(IModRegistry registry) {
@@ -34,10 +32,30 @@ public class JEIPlugin implements IModPlugin {
 				registry.addRecipeCatalyst(new ItemStack(ModuleFeature.porcelainMelter), TINKERS_SMELTERY);
 				registry.addRecipeCatalyst(new ItemStack(ModuleFeature.porcelainMelter, 1, 8), FURNACE_FUEL);
 			}
+
+			// liquid recipe lookup
+			registry.addAdvancedGuiHandlers(new TinkerGuiTankHandler<>(GuiMelter.class));
 		}
 	}
 
-	@Override
-	public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {}
 
+	private static class TinkerGuiTankHandler<T extends GuiContainer & IGuiLiquidTank> implements IAdvancedGuiHandler<T> {
+		private Class<T> clazz;
+
+		public TinkerGuiTankHandler(Class<T> clazz) {
+			this.clazz = clazz;
+		}
+
+		@Nonnull
+		@Override
+		public Class<T> getGuiContainerClass() {
+			return clazz;
+		}
+
+		@Nullable
+		@Override
+		public Object getIngredientUnderMouse(T guiContainer, int mouseX, int mouseY) {
+			return guiContainer.getFluidStackAtPosition(mouseX, mouseY);
+		}
+	}
 }
