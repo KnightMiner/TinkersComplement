9990eb6a06462c07d9a9626f389a40c32ab4172f
KnightMiner <knightminer4@gmail.com>
2017-12-11 08:53:50 -0600

    Fix crash on server when loading armor (#9)

--- src/main/java/knightminer/tcomplement/feature/items/ItemArmorBase.java
+++ src/main/java/knightminer/tcomplement/feature/items/ItemArmorBase.java
@@ -1,31 +1,13 @@
 package knightminer.tcomplement.feature.items;
 
 import knightminer.tcomplement.library.TCompRegistry;
-import net.minecraft.entity.Entity;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemArmorBase extends ItemArmor {
 
-	private ResourceLocation texture;
 	public ItemArmorBase(ArmorMaterial material, EntityEquipmentSlot slot) {
 		super(material, 0, slot);
 		this.setCreativeTab(TCompRegistry.tabGeneral);
-		this.texture = new ResourceLocation(material.getName());
 	}
-
-	@SideOnly(Side.CLIENT)
-	@Override
-	public String getArmorTexture(ItemStack stack, Entity entity, EntityEquipmentSlot slot, String type) {
-		if(slot == EntityEquipmentSlot.LEGS) {
-			return String.format("%s:textures/models/armor/%s_leggings.png", texture.getResourceDomain(), texture.getResourcePath());
-		}
-
-		return String.format("%s:textures/models/armor/%s.png", texture.getResourceDomain(), texture.getResourcePath());
-	}
-
 }
