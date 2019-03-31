478bdc5a7176398118a69f8cf40c00fcb2339b12
KnightMiner <knightminer4@gmail.com>
2019-03-31 02:57:49 -0500

    Update to lastest Mantle build and replace ModuleFileRepository with the Mantle class

--- src/main/java/knightminer/tcomplement/shared/CommonsClientProxy.java
+++ src/main/java/knightminer/tcomplement/shared/CommonsClientProxy.java
@@ -1,19 +1,20 @@
 package knightminer.tcomplement.shared;
 
+import knightminer.tcomplement.TinkersComplement;
 import knightminer.tcomplement.common.ClientProxy;
 import knightminer.tcomplement.library.Util;
-import knightminer.tcomplement.shared.client.ModuleFileRepository;
 import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import slimeknights.mantle.client.book.repository.ModuleFileRepository;
 import slimeknights.tconstruct.library.book.TinkerBook;
 
 public class CommonsClientProxy extends ClientProxy {
 	@Override
 	public void preInit() {
 		super.preInit();
-		TinkerBook.INSTANCE.addRepository(new ModuleFileRepository(Util.resource("book")));
+		TinkerBook.INSTANCE.addRepository(new ModuleFileRepository(TinkersComplement.pulseManager, Util.resource("book")));
 	}
 
 	@SubscribeEvent
